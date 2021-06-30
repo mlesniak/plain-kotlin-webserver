0962e5550a731ab3e725745df17327afe92a8706
Michael Lesniak <mail@mlesniak.com>
2021-06-30 21:21:34 +0200

    Accept incoming connection

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,22 @@
+import java.net.ServerSocket
+
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Hello, world")
+            val port = 8080
+
+            println("Listening on port $port")
+            val ss = ServerSocket(port)
+            val client = ss.accept()
+
+            val istream = client.getInputStream()
+            val bs = istream.readAllBytes()
+            val content = String(bs)
+            println(content)
+
+            val ostream = client.getOutputStream()
+            ostream.write("Bye".encodeToByteArray())
         }
     }
 }
