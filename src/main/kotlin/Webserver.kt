31cd0fc062c45fadb22d920da4e71b26c4d64479
Michael Lesniak <mail@mlesniak.com>
2021-07-07 07:18:10 +0200

    Refactoring

--- src/main/kotlin/Webserver.kt
+++ src/main/kotlin/Webserver.kt
@@ -11,6 +11,9 @@ class WebServer(private val port: Int = 8080) {
     private val log: Log = Log.of(WebServer::class.java)
     private val handlers: MutableMap<HttpRequest, HttpHandler> = mutableMapOf()
 
+    /**
+     * Entry function to start listening for connections.
+     **/
     fun start() {
         log.info("Listening on port $port")
         val socket = ServerSocket(port)
@@ -25,18 +28,26 @@ class WebServer(private val port: Int = 8080) {
                 val response = HttpResponse()
                 client.getOutputStream().use { os ->
                     handler(request, response)
-                    writeOutput(os, response)
+                    sendHTTPResponse(os, response)
                 }
-            } ?: IllegalArgumentException("No handler for $request")
+            } ?: IllegalArgumentException("No handler found for $request")
         }
     }
 
-    private fun writeOutput(os: OutputStream, response: HttpResponse) {
-        os.write("HTTP/1.1 ${response.status} OK\n".toByteArray())
+    private fun sendHTTPResponse(os: OutputStream, response: HttpResponse) {
         val bos = response.outputStream as ByteArrayOutputStream
-        os.write("Content-Length: ${bos.size()}\n".toByteArray())
-        os.write("\n".toByteArray())
+
+        os.writeLine("HTTP/1.1 ${response.status} OK")
+        os.writeLine("Content-Length: ${bos.size()}")
+        os.writeLine()
+
         os.write(bos.toByteArray())
+        bos.close()
+    }
+
+    private fun OutputStream.writeLine(s: String = "") {
+        val snl = s + "\n"
+        this.write(snl.toByteArray())
     }
 
     fun handle(method: HttpMethod, path: String, handler: (HttpRequest, HttpResponse) -> Unit) {
